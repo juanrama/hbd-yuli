eafe4163c64f87faf3c33b5958da8f90f05d29ea
juanrama <115603059+juanrama@users.noreply.github.com>
2023-04-07 22:52:32 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,31 @@
+
+var config = {
+    texts: [
+        "Happy Sweet Seventeen",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "AYANG",  // 同上...
+        "SEMOGA KAMU PANJANG UMUR",
+        "MAKIN SAYANG SAMA AKUU",
+        "DAN MAKIN CINTA SAMA AKUU :P",
+        "TAMBAH PINTER LAGI YA",
+        "TAMBAH DEWASA JUGA",
+        "JANGAN SERING NGELAMUN LGI",
+        "DAN JGN SERING EMOSI LGI",
+        "MWAAHH",
+        "LOVE YOU",
+    ],
+    
+    imgs: {
+        "AYANG": "./imgs/pekok.jpg",
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+    desc: {
+        turn_on: "PENCET YA BUAT NYALAIN",
+        play: "MULAI",
+        bannar_coming: "KOK SEPI, KLIK LG DONG",
+        balloons_flying: "DEKORIN YANG",
+        cake_fadein: "ADA KUENYA LOH ？",
+        light_candle: "DITIUP YA LILINYA ？",
+        wish_message: "HBD SAYANGKU :P",
+        story: "A MESSAGE FOR YOU",
+    }
+};
